1b1187a4b139cfd36893f2b225c31fb92d68ec34
jean-roger orsini <jean-roger@VR-MC040-3.local>
2017-11-15 15:59:21 +0900

    Added common Phrases page.

---
+++ common-phrases.js
@@ -0,0 +1,290 @@
+let current = {
+	occured:[],	
+	/**
+	 * create id for the question and checks if it's already in there
+	 */
+	createQuestionId: function(fillInFunc) {
+
+		var objectLen = Object.keys(commonPhrases).length,
+			id = Math.floor( Math.random() * objectLen ) + 1
+
+		while(this.occured.indexOf(id) !== -1) {
+
+			id = Math.floor( Math.random() * objectLen ) + 1
+		}
+
+		this.occured.push(id)
+
+		if (this.occured.length === objectLen) {
+
+			this.occured = []
+		}
+		return id
+	},
+	/**
+	 * Updates the current object.
+	 */
+	fillIn: function() {
+		
+		let id = this.createQuestionId()
+		view.question = commonPhrases[id].question,
+		view.options = commonPhrases[id].options,
+		view.stats = commonPhrases[id].stats,
+		view.userInput = commonPhrases[id].user_input,
+		view.graspLevel = commonPhrases[id].grasp_level,
+		view.answer = commonPhrases[id].options.map(e => ( e[1] === true) ? e[0] : '').join('')
+	}
+}
+
+let view = {
+	userStats: {right: 0, wrong:0},
+	/**
+	 * Displays sentence and add sentence in the occurred array
+	 */
+	showQuestion: function() {
+
+		let applicableElement = $('#sentence');
+
+		applicableElement.html(`<span>${this.question[0]}</span><input type="text" id="option" onkeyup="checkAnswer(event)"><span>${this.question[1]}</span>`)
+		this.updateGraspLevel()
+		wanakana.bind($('#sentence')[0]);		
+	},
+
+	/**
+	 * Sets the 'options' variable and displays options 
+	 */
+	showOptions: function() {
+
+		$('.assignment').html(`<u>Choose between</u><br><b>${this.options.map(e => e[0]).join('</b> - <b>')}</b>`)
+	},
+	/**
+	 * Updates the repetition stats
+	 */
+	showRepetitions: function() {
+
+		let repetitions = this.userStats.right + this.userStats.wrong,
+			percentage = repetitions === 0 ? 0 : Math.round(this.userStats.right / repetitions * 100)
+
+		$('#repetition').html(` ${repetitions}`)
+		$('#success').html(` ${this.userStats.right}`)
+		$('#failure').html(` ${this.userStats.wrong}`)
+		$('#percentage').html(` ${percentage}%`)
+	},
+	/**
+	 * Reset Repetitions to 0
+	 */
+	resetRepetitions: function() {
+		console.log('test')
+		this.userStats.right = 0
+		this.userStats.fail = 0
+		this.showRepetitions()
+	},
+	/**
+	 * Attaches a function to an element
+	 */
+	attachEvent: function(element, type, func) {
+		element.addEventListener(type, func)
+	},
+	/**
+	 * Updates question's grasp level
+	 */
+	updateGraspLevel: function () {
+	
+		if ($('.sentence')[0].classList.length > 1) {
+	
+			$('.sentence').removeClass( $('.sentence')[0].classList[1] );
+		}
+	
+		$('.sentence').addClass(`grasp--${this.graspLevel}`);
+	},
+
+	/**
+	 * Checks if the input matches with any options 
+	 */
+	optionMatch: function(userInput) { // Checks the input value, sees if it match any of the prepostions.
+		if(userInput !== '') {
+
+			for(var i = 0, len = this.options.length; i < len; i++){
+				
+				if(userInput.trim() === this.options[i][0]) { return true }
+			}
+		}
+		return false
+	}
+	
+};
+
+var 
+	// Collects ids that have been already shown.
+	occured = [],
+	// To retrieve the user input value.
+	inputEl = $('#option')[0],
+	// Question's length
+	len = Object.keys(commonPhrases).length,
+	// Stat's object
+	stats = {
+		success:0,
+		fail:0
+	}
+
+function isNew() {
+	console.log(curr)
+	if(curr.stats.length !== undefined) {
+		$('#new_icon').html('')
+	} else {
+		$('#new_icon').html('star_border')
+	}
+}
+
+/**
+ * Checks if the user's input match with the answer
+ */
+function checkAnswer(e, inputVal = $('#option')[0].value) { // Checks input value to the answer.		
+	if(e.keyCode === 13 && view.optionMatch(inputVal)) {
+
+		if(inputVal !== view.answer) {
+			view.userStats.fail++			
+			if(view.userInput[inputVal]) {
+
+				view.userInput[inputVal]++
+				view.stats.fail++
+
+				if(view.userInput[inputVal] > 2) {
+
+					Materialize.toast(
+						`You made that mistake &nbsp;
+						<b>${view.userInput[inputVal]}</b>&nbsp; 
+						times with &nbsp;Åu
+						<b>${inputVal}</b>Åv, be careful`,
+						4000
+					)
+				}
+			} else {
+
+				view.userInput[inputVal] = 1
+			}
+		} else {
+			view.userStats.right++
+			view.stats.success++
+			view.lvl++
+		}
+
+		layoutUpdate(inputVal === view.answer)		
+
+		if(inputVal === view.answer && match_grammar_point()){
+
+			var g_point = match_grammar_point()
+
+			$('#see-more').html(
+				`${view.question[0]}<b>${view.answer}</b>${view.question[1]}
+				<br>
+				<a target="_blank" id="see-more_link" href="${g_point[1]}">
+				See more about <b>${g_point[0]}</b></a>`
+			)
+		}
+		
+		localstorageUpdate()
+	}
+}
+
+
+
+/**
+ * Check if the answer matches a grammatical point.
+ * And returns it
+ */
+function match_grammar_point() {
+	var match_len = 0, grammar_point;
+
+	for (var i = 0; i < jlpt3_grammar_list.length; i++) {
+
+		var re = RegExp(jlpt3_grammar_list[i][0],'g')
+
+		if(view.answer.match(re)!==null && view.answer.match(re).length > match_len){
+
+			match_len = view.answer.match(re).length
+			grammar_point = jlpt3_grammar_list[i]
+		}
+	}
+
+	return match_len > 0 ? grammar_point : false
+}
+
+/**
+ * Check if the answer matches a grammatical point.
+ * And returns it
+ */
+function layoutUpdate (output) {
+	var successMsg 	= 'Right on!',
+		errorMsg	= 'Try again!',
+		option_id	= view.options.map(e => e[0]).indexOf($('#option')[0].value)
+	view.stats.count++
+	if(output === true) {
+
+		Materialize.toast(successMsg, 1000)
+		$('.assignment b').removeClass('prep--missed')
+		$('.sentence').addClass('sentence--success')
+		$('#definition').addClass('definition--success')
+
+		setTimeout( function() {
+
+			$('.sentence').removeClass('sentence--success') 
+		}, 500)
+
+	} else {
+
+		Materialize.toast(errorMsg, 1000)
+		$('.assignment b').eq(option_id).removeClass('prep--missed').addClass('prep--missed')
+		$('.sentence').addClass('shaky')
+		
+		setTimeout( function() {
+
+			$('.sentence').removeClass('shaky') 
+		}, 820);
+		
+		$('#option')[0].value = ''
+		if(commonPhrases[curr.id].lvl > 0){
+
+			commonPhrases[curr.id].lvl++
+		}
+	}
+	view.showRepetitions()
+}
+
+
+/**
+ * Sets status and questions into localstorage.
+ */
+function localstorageSetUp(name, object) {
+
+	if(localStorage.getItem(name) === null) {
+
+		localStorage.setItem(name, JSON.stringify(object))
+	} 
+
+	return JSON.parse( localStorage.getItem(name) )
+}
+
+/**
+ * localstorageUpdate() Set the the updated data set version to localstorage
+ */
+function localstorageUpdate() {
+
+	localStorage.setItem( 'jlpt3-grammar', JSON.stringify(commonPhrases) )
+	localStorage.setItem( 'jlpt3-grammar-stats', JSON.stringify(stats) )
+	
+	commonPhrases = localstorageSetUp('jlpt3-grammar',commonPhrases)
+	stats = localstorageSetUp('jlpt3-grammar-stats',stats)	
+}
+
+// Set input field to Hiragana characters
+
+commonPhrases = localstorageSetUp('jlpt3-grammar',commonPhrases)
+stats = localstorageSetUp('jlpt3-grammar-stats',stats)	
+localstorageUpdate()
+current.fillIn()
+view.showRepetitions()
+view.showQuestion()
+view.showOptions()
+
+// view.attachEvent($('#repResetIcon')[0],'click',view.resetRepetitions)
---
+++ commonPhrases.js
@@ -0,0 +1,204 @@
+var commonPhrases = [
+    {
+        "english": "Are you sure?",
+        "japanese": [
+            "本当に?",
+            "確かか?"
+        ],
+        "romaji": [
+            "hontou ni.",
+            "tashika ka."
+        ]
+    },
+    {
+        "english": "No, that’s not possible.",
+        "japanese": [
+            "まさか、ありえないよ.",
+            "まさか、そんなことありっこない."
+        ],
+        "romaji": [
+            "masaka, arienai yo.",
+            "masaka, sonna koto arikkonai."
+        ]
+    },
+    {
+        "english": "Don’t be ridiculous.",
+        "japanese": [
+            "冗談はおよしなさい."
+        ],
+        "romaji": [
+            "joudan wa oyoshi nasai."
+        ]
+    },
+    {
+        "english": "It seem quite unlikely.",
+        "japanese": [
+            "その可能性はきわめて低い."
+        ],
+        "romaji": [
+            "sono kanousei wa kiwamete hikui."
+        ]
+    },
+    {
+        "english": "You sure you weren’t dreaming?",
+        "japanese": [
+            "夢でも見たんじゃないのか?"
+        ],
+        "romaji": [
+            "yume demo mitan janai no ka."
+        ]
+    },
+    {
+        "english": "That’s hardly something to joke about.",
+        "japanese": [
+            "そんなことを、冗談にも言ってはいけないわ."
+        ],
+        "romaji": [
+            "sonna koto o, joudan ni mo itte wa ikenai wa."
+        ]
+    },
+    {
+        "english": "Had it really happened?",
+        "japanese": [
+            "本当に起こったことだろうか?"
+        ],
+        "romaji": [
+            "hontou ni okotta koto darou ka."
+        ]
+    },
+    {
+        "english": "Is that even possible?",
+        "japanese": [
+            "そんなことがありうるだろうか?"
+        ],
+        "romaji": [
+            "sonna koto ga ariuru darou ka."
+        ]
+    },
+    {
+        "english": "I’m not sure this is going to work.",
+        "japanese": [
+            "そんなにうまくいくとは思えないけど."
+        ],
+        "romaji": [
+            "sonna ni umaku iku to wa omoenai kedo."
+        ]
+    },
+    {
+        "english": "AdvertisementAre you sure you’ve thought this through?",
+        "japanese": [
+            "ほんとに、このこと、ちゃんと考えたの?"
+        ],
+        "romaji": [
+            "honto ni, kono koto, chanto kangaeta no."
+        ]
+    },
+    {
+        "english": "Did you really say that?",
+        "japanese": [
+            "本当にそう言ったの?"
+        ],
+        "romaji": [
+            "hontou ni sou itta no."
+        ]
+    },
+    {
+        "english": "Are you sure this is the right way?",
+        "japanese": [
+            "この道で間違いないかい?"
+        ],
+        "romaji": [
+            "kono michi de machigai nai kai."
+        ]
+    },
+    {
+        "english": "I’m not sure if that’s a compliment.",
+        "japanese": [
+            "それって褒められているのかしら."
+        ],
+        "romaji": [
+            "sore tte homerarete iru no kashira."
+        ]
+    },
+    {
+        "english": "Nothing else can have happened, can it?",
+        "japanese": [
+            "また何か起こるなんて、ありえないよね?"
+        ],
+        "romaji": [
+            "mata nanika okoru nante, arienai yo ne."
+        ]
+    },
+    {
+        "english": "But we’re not sure if that’s true.",
+        "japanese": [
+            "だけど、ほんとうかどうか、私たちにはわからないのよ."
+        ],
+        "romaji": [
+            "da kedo, hontou ka dou ka, watashi tachi ni wa wakaranai no yo."
+        ]
+    },
+    {
+        "english": "I have some doubt about his reliability.",
+        "japanese": [
+            "彼が信頼できるかどうか、少し心配なところがある."
+        ],
+        "romaji": [
+            "kare ga shinrai dekiru ka dou ka, sukoshi shinpai na tokoro ga aru."
+        ]
+    },
+    {
+        "english": "Surely that’s a little bit of an overstatement?",
+        "japanese": [
+            "まさか、それはちょっと大げさじゃないか?"
+        ],
+        "romaji": [
+            "masaka, sore wa chotto oogesa ja nai ka."
+        ]
+    },
+    {
+        "english": "You’re not actually disappointed, are you?",
+        "japanese": [
+            "まさか、がっかりしてるんじゃないだろうな?"
+        ],
+        "romaji": [
+            "masaka, gakkari shiterun janai darou na."
+        ]
+    },
+    {
+        "english": "It’s hard to know what to believe these days.",
+        "japanese": [
+            "最近は、何を信じたらいいか知るのが難しい."
+        ],
+        "romaji": [
+            "saikin wa, nani o shinjitara ii ka shiru no ga muzukashii."
+        ]
+    },
+    {
+        "english": "Are you sure about this?",
+        "japanese": [
+            "ね、ほんとにそうなの?"
+        ],
+        "romaji": [
+            "ne, honto ni sou na no."
+        ]
+    },
+    {
+        "english": "That sounds fishy.",
+        "japanese": [
+            "うさんくさいなあ."
+        ],
+        "romaji": [
+            "usankusai naa."
+        ]
+    },
+    {
+        "english": "Nonsense!",
+        "japanese": [
+            "ナンセンス!"
+        ],
+        "romaji": [
+            "nansensu."
+        ]
+    }
+]
